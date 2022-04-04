8b423024d1b65ecb347e674795745fc25e99b142
Rares5242 <rares5242@gmail.com>
2022-04-04 17:48:34 +0300

    commit

---
+++ Lab08/ex3/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+app.use(express.urlencoded({ extended: true }));
+app.get('/', function (req, res) {
+    res.sendFile('index.html', { root: __dirname });
+});
+app.get('/mac', function (req, res) {
+    res.sendFile('mac.svg', { root: __dirname });
+});
+app.post('/stud17', function (req, res) {
+    console.log(req.method + ' ' + req.url);
+    console.log(req.body);
+    res.send('data received');
+});
+var server = app.listen(3017, function () {
+    console.log('Node server is running..');
+});
